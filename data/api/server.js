f3bcb989c2f585a40d20c8b822c95f37b6885bcf
Evans Ibok <evansibok@gmail.com>
2020-01-28 18:01:21 +0100

    Create encompassing routes file

--- data/api/server.js
+++ data/api/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const usersRouter = require('../users/users-router');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use('/api/users', usersRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).json(`API is live`);
-})
+app.use('/api', routes);
 
 module.exports = app;
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/users.db3'
+      filename: './data/auth.db3'
     },
     migrations: {
       directory: './data/migrations',
